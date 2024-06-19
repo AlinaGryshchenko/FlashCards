972116e794d7c369b5370fde3f1a53eaae45b0d3
AlinaGryshchenko <alinahryshchenkoios@gmail.com>
2024-06-19 11:35:58 +0100

    Initial Commit

---
+++ FlashCards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashCards
+//
+//  Created by Алина Лошакова on 19.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
